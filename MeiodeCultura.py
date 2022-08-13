fffae187b4782427a854fe03329b88c5ecb3b9fc
Antenor-L-Neto <81195678+Antenor-L-Neto@users.noreply.github.com>
2022-08-13 09:59:52 -0300

    Microorganism medium - Kirk Medium
    This is a code which can be used to define the reagents quantities necessary to make a microorganism medium, based on the desired volume.

---
+++ MeiodeCultura.py
@@ -0,0 +1,19 @@
+# Cálculo de meio de cultura Kirk
+
+
+Reagentes= {"Dextrose":"5","Extr.Levedura":"2","KH2PO4":"0.2","MgSO4":"0.05","CaCl2":"0.013","MnSO4":"0.016", "CuSO4":"0.0499" }
+
+Quant = input("Digite quanto você precisa de meio em L")
+print( )
+print("--------------")
+List = []
+indexx = 0
+for i in Reagentes:
+    List.append(float(Reagentes[i])*float(Quant));
+for i in Reagentes:
+    print("Para o reagente {} é necessário {} gramas".format(i,List[indexx]))
+    print("--------------")
+    indexx+=1
+
+
+input()
